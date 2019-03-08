296d46d046996d90392242ffababbb7daa998c22
mimikun <karaage_nennen@icloud.com>
2019-03-08 23:25:31 +0900

    Update: update_pkg_list.sh

--- update_pkg_list.sh
+++ update_pkg_list.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-
+  
 sudo apt update
 sudo apt list --upgradable | less
+echo "upgrade pkgs [Y/n]"?
+read ANSWER
+
+case $ANSWER in
+    "Y" | "y" | "yes" | "Yes" | "YES" ) sudo apt upgrade -y;;
+    * ) ;;
+esac
