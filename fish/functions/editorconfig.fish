77120aa5715193b18de28d007642356678720110
Yuto Tanaka <karaage_nennen@icloud.com>
2020-08-03 17:15:16 +0900

    Add some fisher packages

--- fish/functions/editorconfig.fish
+++
@@ -1,8 +0,0 @@
-function editorconfig --description 'Generate editorconfig'
-  if not test -e .editorconfig
-    echo ".editorconfig not exist."
-    echo "Creating .editorconfig."
-    cp $DOTFILES/.editorconfig-mimikun ./.editorconfig
-  end
-end
-
--- fish/functions/gcd.fish
+++
@@ -1,6 +0,0 @@
-function gcd --wraps='cd (ghq list -p | fzf)' --description 'cd using ghq and fzf'
-  ghq list -p | fzf | read select
-  [ -n "$select" ]; and cd "$select"
-  echo " $select"
-  commandline -f repaint
-end
